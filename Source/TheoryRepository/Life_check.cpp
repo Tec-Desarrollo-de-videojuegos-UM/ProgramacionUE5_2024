0566a4436bc2426ce64c12d4d2d350ef1b6f9ed0
oberdan39 <nachoobacha@gmail.com>
2024-05-23 22:19:59 -0300

    ,

---
+++ Source/TheoryRepository/Life_check.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Life_check.h"
+
+Life_check::Life_check()
+{
+}
+
+Life_check::~Life_check()
+{
+}
---
+++ Source/TheoryRepository/Life_check.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class THEORYREPOSITORY_API Life_check
+{
+public:
+	Life_check();
+	~Life_check();
+};
