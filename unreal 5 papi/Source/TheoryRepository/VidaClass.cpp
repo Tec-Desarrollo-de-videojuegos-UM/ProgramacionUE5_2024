80161ed0e4bd4ef0c22daa1c34095eecfb53e5dc
Proyectxd <164433790+Proyectxd@users.noreply.github.com>
2024-06-02 18:46:46 -0300

    update

---
+++ unreal 5 papi/Source/TheoryRepository/VidaClass.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VidaClass.h"
+#include <iostream>
+
+// Sets default values for this component's properties
+UVidaClass::UVidaClass()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UVidaClass::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UVidaClass::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+int main(){
+float vida;
+vida = 100;
+if (vida >= 1;)
+std::cout << "El personaje sigue vivo" << std::enld;
+if (vida <= 0;)
+std::cout << "El personaje esta muerto" << std::enld;
+}
