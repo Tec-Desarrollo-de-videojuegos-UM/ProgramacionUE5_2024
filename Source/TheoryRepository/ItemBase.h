eacc173c0478791f3201bddaa4a05c3f66888d81
maximocodatto <164428060+maximocodatto@users.noreply.github.com>
2024-06-03 15:19:32 -0300

    actividad

---
+++ Source/TheoryRepository/ItemBase.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBase.generated.h"
+
+UCLASS()
+class THEORYREPOSITORY_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemBase();
+	//called every frane
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable)
+	FString ChangeName(FString NewName);
+	UFUNCTION(BlueprintCallable)
+	int32 ChangeAmount(int32 NewAmount);
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	FString name;
+	UPROPERTY(BlueprintReadOnly)
+	int32 amount;
+	
+
+};
