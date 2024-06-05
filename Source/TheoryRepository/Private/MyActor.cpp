eb9f50ad93e7187d98be1d66c949099286ba7581
Edwin-OwO <nicolaslabrune@gmail.com>
2024-06-05 17:24:32 -0300

    no entiendo xd

---
+++ Source/TheoryRepository/Private/MyActor.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollitionBox"));
+}
+
+// Called when the game starts or when spawned
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
