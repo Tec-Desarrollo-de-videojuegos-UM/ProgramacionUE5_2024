ddfaaa8357b6a5b8b445c3f4cb34db284ac7424d
Lelox32 <matifradin@gmail.com>
2024-06-06 18:03:28 -0300

    Updated Class

---
+++ Source/TheoryRepository/Itembase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Myactor2.h"
+#include "Itembase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEORYREPOSITORY_API AItembase : public AMyactor2
+{
+	GENERATED_BODY()
+	
+};
