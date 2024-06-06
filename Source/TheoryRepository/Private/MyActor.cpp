ffdeefb498e2a062e28925b0925b3b38abf5b74d
Edwin-OwO <nicolaslabrune@gmail.com>
2024-06-05 23:22:44 -0300

    updated

--- Source/TheoryRepository/Private/MyActor.cpp
+++ Source/TheoryRepository/Private/MyActor.cpp
@@ -15,9 +15,54 @@ AMyActor::AMyActor()
 
 // Called when the game starts or when spawned
 
+// check heatlh
+
+void CheckHealth(int Health) {
+
+	if (Health <= 0) {
+
+		std::cout << " player is dead " << std::endl;
+	}
+	else {
+
+		std::cout << " player is alive " << std::endl;
+	}
+
+
+}
+
+void CheckEnergy(int& Energy, int energyrequierd) {
+
+	if (Energy >= energyrequierd) {
+		Energy -= energyrequierd;
+		std::cout << " Action can be done " << std::endl;
+	}
+	else {
+		std::cout << " Action can´t be done " << std::endl;
+	}
+}
+
+int main() {
+
+	int HealthCharacter = 50;
+	CheckHealth;
+
+	int playerenergy = 100;
+	int energyforaction = 40;
+
+	CheckEnergy(playerenergy, energyforaction);
+
+	return 0;
+
+
+}
+
+
 // Called every frame
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
+
+
