81c3fa6a5d909bf40179fbc0032c33624616f00c
Vicky Cardaci <vickycardaci04@gmail.com>
2024-06-11 22:20:37 -0300

    Revert "Delete ( I still don't understand)"
    This reverts commit fa1cab26518077d3402548cfb4b68358b0988eb8.

---
+++ Source/TheoryRepository/RecipeWidget.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RecipeWidget.h"
+#include "Components/UniformGridPanel.h"
+#include "Components/TextBlock.h"
+
+URecipeWidget::URecipeWidget(const FObjectInitializer& ObjectInicilizer) : 
+	Super(ObjectInicilizer)
+{
+	
+}
+
+void URecipeWidget::SetRecipe(TMap<FString, int32>& Recipe)
+{
+	RecipeMap = Recipe;
+
+	if (!RecipeGrid)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("RecipeGrid is not bound in the UI."));
+		return;
+	}
+
+	RecipeGrid->ClearChildren();
+
+	int32 RowIndex = 0;
+	for (const TPair<FString, int32>& IngredientPair : RecipeMap)
+	{
+		UTextBlock* IngredientText = NewObject<UTextBlock>(this);
+		if (IngredientText)
+		{
+			IngredientText->SetText(FText::FromString(IngredientPair.Key));
+			RecipeGrid->AddChildToUniformGrid(IngredientText, RowIndex, 0);
+		}
+
+		UTextBlock* QuantityText = NewObject<UTextBlock>(this);
+		if (QuantityText)
+		{
+			QuantityText->SetText(FText::AsNumber(IngredientPair.Value));
+			RecipeGrid->AddChildToUniformGrid(QuantityText, RowIndex, 1);
+		}
+
+		RowIndex++;
+	}
+}
+
+void URecipeWidget::AddIngredient(FString ingredientName, int32 Quantity)
+{
+	RecipeMap.Add(ingredientName, Quantity);
+
+	SetRecipe(RecipeMap);
+}
