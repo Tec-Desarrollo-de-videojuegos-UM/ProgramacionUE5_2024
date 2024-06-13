fe64e13707b9cc5bafd5bfdb3b1d5913365965ca
Batumo01 <164427993+Batumo01@users.noreply.github.com>
2024-06-12 22:07:23 -0300

    si

---
+++ Source/TheoryRepository/AtributesComponent.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealtComponent.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class THEORYREPOSITORY_API UHealtComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UHealtComponent();
+
+	UPROPERTY(BlueprintCallable)
+	void CheckHealt();
+	UPROPERTY(BlueprintCallable)
+	void UseStamina(float StaminaToUse);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float health;
+
+	UPROPERTY(EditAnywhere)
+	float stamina;
+};
