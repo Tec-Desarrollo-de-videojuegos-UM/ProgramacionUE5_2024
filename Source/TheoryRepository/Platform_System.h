9c0df26e967b557418c383d978eb326ad0f225da
Lelox32 <matifradin@gmail.com>
2024-06-13 19:06:02 -0300

    Created Platform Class

---
+++ Source/TheoryRepository/Platform_System.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Myactor2.h"
+#include "Platform_System.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEORYREPOSITORY_API APlatform_System : public AMyactor2
+{
+	GENERATED_BODY()
+	
+};
