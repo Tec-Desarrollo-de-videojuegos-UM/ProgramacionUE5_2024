9247c771162141d3d387fa9dfc99ae0e8b36f44b
JulietaUzair <julietamuzair@gmail.com>
2024-06-12 23:04:33 -0300

    UpdateActivity1

---
+++ Source/TheoryRepository/AtributteState.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyClass.h"
+#include "AtributteState.h"
+
+// Sets default values for this component's properties
+UAtributteState::UAtributteState()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+//Creamos las funciones en el cpp
+
+void UAtributteState::CheckHealth()
+{
+	if (health >= 0)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue, TEXT("El personaje esta vivo"));
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, TEXT("El personaje esta muerto"));
+	}
+	
+}
+
+bool UAtributteState::UseEnergy(float EnergyToUse)
+{
+	if (energy <= 0)
+	{
+		
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue, TEXT("No hay suficiente energia"));
+		return false;
+	}    
+	else
+	{
+		energy = energy - EnergyToUse;
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, TEXT("Se puede realizar la acción"));
+	}
+	
+}
+
+
+
+// Called when the game starts
+void UAtributteState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UAtributteState::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/TheoryRepository/AtributteState.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AtributteState.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class THEORYREPOSITORY_API UAtributteState : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAtributteState();
+
+	//funciones
+	UFUNCTION(BlueprintCallable)
+	void CheckHealth();
+	UFUNCTION(BlueprintCallable)
+	void UseEnergy(float EnergyToUse);
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	//variables o atributos
+    UPROPERTY(EditAnywhere)
+	float health;
+	UPROPERTY(EditAnywhere)
+	float energy;
+
+
+
+		
+};
