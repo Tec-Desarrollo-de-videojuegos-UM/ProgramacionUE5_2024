d492e1ed8cc17b750add7c87d95c197f5f586811
Federico Mora <fedenahuel566@gmail.com>
2024-06-13 22:17:06 -0300

    TrabajoPractico3

---
+++ Source/TheoryRepository/HealthComponent.cpp
@@ -0,0 +1,38 @@
+#include "HealthComponent.h"
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+	health = 100;
+
+}
+
+bool UHealthComponent::HealthStatus()
+{
+
+	if (health == 0)
+	{
+
+		destruction = true;
+
+	}
+	else
+	{
+
+		destruction = false;
+
+	}
+
+	return false;
+}
+
+
---
+++ Source/TheoryRepository/HitPlatform.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <Components/BoxComponent.h>
+#include "HitPlatform.generated.h"
+
+UCLASS()
+class THEORYREPOSITORY_API AHitPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHitPlatform();
+
+
+private:
+	//variables
+	UPROPERTY (EditAnywhere)
+	UBoxComponent* TriggerBox;
+
+};
