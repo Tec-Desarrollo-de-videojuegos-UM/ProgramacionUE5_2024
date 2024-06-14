fbfc09e1ecbd60a354538710ba5f377b7672a520
DIAMONJET <migonitiago362@gmail.com>
2024-06-14 18:27:42 -0300

    TP N°2 Started Functionality
    Done:
    Activity 2: B

--- Source/TheoryRepository/HitPlatform.cpp
+++ Source/TheoryRepository/HitPlatform.cpp
@@ -9,6 +9,10 @@ AHitPlatform::AHitPlatform()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollider"));
+	BoxMesh = CreateDefaultSubobject<UStaticMesh>(TEXT("BoxMesh"));
+
+	RootComponent = BoxCollider;
 }
 
 // Called when the game starts or when spawned
