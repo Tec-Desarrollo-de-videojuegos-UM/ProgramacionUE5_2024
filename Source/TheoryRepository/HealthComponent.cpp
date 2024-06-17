46050571e051ac3cdf0307ebf6b02172536bc50c
Aslaeur <gabrielrauss@yahoo.com>
2024-06-17 16:42:36 -0300

    intento de actividad 3

---
+++ Source/TheoryRepository/HealthComponent.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+#include "AtributteState.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+bool UHealthComponent::CheckHealth()
+{
+	if (Health > 0)
+	{
+		GEngine->BeginDestroy();
+	}
+	else
+	{
+
+	}
+};
+
