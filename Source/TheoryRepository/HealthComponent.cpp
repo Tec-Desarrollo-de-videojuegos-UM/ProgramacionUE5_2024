b407285397aa00dec29e530df5cb7d226b2744f1
SantinoPadilla <santixxpro@gmail.com>
2024-06-17 19:30:08 -0300

    Upload Platform Activity
    La actividad esta en el ThirPersonMap.

---
+++ Source/TheoryRepository/HealthComponent.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+
+bool UHealthComponent::CanDestroy()
+{
+	bool canDestroy;
+
+	if (life <1)
+	{
+		canDestroy = true;
+	}
+	else
+	{
+		canDestroy = false;
+
+	}
+	return canDestroy;
+}
+
+void UHealthComponent::TakeDamage()
+{
+	life = life - 5;
+	
+}
+
+
+
+
