6fd65fbd63c9b776298328cc99df75894737862e
FacundoNimes <facunimes@gmail.com>
2024-06-18 15:26:40 -0300

    Practico 2 Plataforma

---
+++ Source/TheoryRepository/HealthComponent.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+bool UHealthComponent::CheckPlatformHealth()
+{
+
+	platformHealth = platformHealth - 25;
+	if (platformHealth > 0)
+	{
+		return true;
+	}
+	else
+	{
+	
+		return false;
+	}
+}
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/TheoryRepository/HitPlatform.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitPlatform.h"
+
+// Sets default values
+AHitPlatform::AHitPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	PlatformCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("PlatformCollision"));
+
+	RootComponent = PlatformCollider;
+}
