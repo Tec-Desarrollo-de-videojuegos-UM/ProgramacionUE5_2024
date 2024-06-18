b27aae3d35dc68e0e9e49d78e09af11a7c49952d
FranciscoMontivero <franciscojmontivero@gmail.com>
2024-06-18 15:09:54 -0300

    TP2 Terminado

---
+++ Source/TheoryRepository/HitPlatform.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitPlatform.h"
+
+// Sets default values
+AHitPlatform::AHitPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+
+	RootComponent = BoxCollider;
+}
+
+// Called when the game starts or when spawned
+void AHitPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHitPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
