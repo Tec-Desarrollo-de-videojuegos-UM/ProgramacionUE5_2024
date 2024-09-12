ae0184c7f1b259d0aefeb2e2ec60d6775e7a5bfd
EmanuelTusoli <164425726+EmanuelTusoli@users.noreply.github.com>
2024-09-12 18:47:33 -0300

    g

---
+++ Intermediate/Build/Win64/x64/TheoryRepositoryEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "A:/gith/ProgramacionUE5_2024/Intermediate/Build/Win64/x64/TheoryRepositoryEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
